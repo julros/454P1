4da8cbc6ba4723e0b4af31dfe79e9094af67a02f
julros <julierosen@sbcglobal.net>
2018-02-15 20:29:07 -0800

    Create project1.py
    set up file

---
+++ project1.py
@@ -0,0 +1,22 @@
+def problem1()
+
+def problem2()
+
+
+
+def main()
+  choice '0'
+  while choice != 'q'
+    print("Please choose a problem.")
+    print("Type '1' for the first problem.")
+    print("Type '2' for the second problem.")
+    print("Type 'q' to quit.")
+    choice = input("--")
+
+    if choice == '1'
+      problem1()
+    if choice == '2'
+      problem2()
+    if choice == 'q'
+    
+main()
